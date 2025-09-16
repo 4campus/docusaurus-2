51c8a1105f1c7c3d39fd9d84ec0ec0dab3ff9532
4campus.net <mail.4campus@gmail.com>
2025-09-16 19:47:08 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '4campus.net',
+  title: '4campus',
   tagline: 'Reach. Connect. Transform.',
   url: 'https://4campus.net',
   baseUrl: '/',
