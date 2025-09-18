b1fe562304df89d4097b8a7a65848d4efa9a7281
4campus.net <mail.4campus@gmail.com>
2025-09-18 17:13:40 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/my-son-rising">
+            to="/voices">
             Listen 🔉
           </Link>
         </div>
