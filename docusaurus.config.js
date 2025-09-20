59b3bccc912c8d0e20e91a51818a94eff8d9c180
4campus.net <mail.4campus@gmail.com>
2025-09-20 10:11:36 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,44 +1,44 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '4campus',
+  title: "4campus",
   // tagline: 'Reach. Connect. Transform.',
-  url: 'https://4campus.net',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  url: "https://4campus.net",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
 
-  organizationName: '4campus',
-  projectName: 'docusaurus-2',
+  organizationName: "4campus",
+  projectName: "docusaurus-2",
 
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -46,98 +46,96 @@ const config = {
 
   themeConfig: {
     metadata: [
-      { name: 'og:image', content: '/img/social-preview.png' },
-      { name: 'twitter:card', content: 'summary_large_image' },
-      { name: 'twitter:image', content: '/img/social-preview.png' },
-      { name: 'og:title', content: '4Campus' },
-      { name: 'og:description', content: 'Reach, Connect, Transform' },
+      { name: "og:image", content: "/img/social-preview.png" },
+      { name: "twitter:card", content: "summary_large_image" },
+      { name: "twitter:image", content: "/img/social-preview.png" },
+      { name: "og:title", content: "4Campus" },
+      { name: "og:description", content: "Reach, Connect, Transform" },
     ],
     colorMode: {
-      defaultMode: 'light',
+      defaultMode: "light",
       disableSwitch: false,
       respectPrefersColorScheme: false,
     },
     navbar: {
-      title: '',
+      title: "",
       logo: {
-        alt: '4campus',
-        src: 'img/logo.svg',
+        alt: "4campus",
+        src: "img/logo.svg",
       },
       items: [
         {
-          label: '10 Questions',
-          href: '/docs/questions',
-          position: 'left',
+          label: "Questions?",
+          href: "/docs/questions",
+          position: "left",
+          className: "navbar__item--questions",
         },
         {
-          label: 'Voices',
-          href: '/voices#latest-recordings',
-          position: 'left',
+          label: "Voices",
+          href: "/voices#latest-recordings",
+          position: "left",
         },
         {
-          label: 'Documentary',
-          href: '/documentary',
-          position: 'left',
+          label: "Documentary",
+          href: "/documentary",
+          position: "left",
         },
         {
-          href: 'https://www.bonfire.com/4campus/',
-          label: 'Store',
-          position: 'left',
+          href: "https://www.bonfire.com/4campus/",
+          label: "Store",
+          position: "left",
         },
-        // Desktop About link
         {
-          label: 'About',
-          href: '/give',
-          position: 'left',
-          className: 'navbar__item--about-desktop',
+          label: "About",
+          href: "/give",
+          position: "left",
+          className: "navbar__item--about-desktop",
         },
-        // Mobile About link (in hamburger)
         {
-          label: 'About',
-          href: '/give',
-          position: 'left',
-          className: 'navbar__item--about-mobile',
+          label: "About",
+          href: "/give",
+          position: "left",
+          className: "navbar__item--about-mobile",
         },
-        // The one 'Give' button, now visible on both mobile and desktop
         {
-          label: 'Give',
-          href: 'https://www.paypal.com/donate/?hosted_button_id=53SQPWUPYSYQQ',
-          position: 'right',
-          className: 'navbar__item--give-mobile',
+          label: "Give",
+          href: "https://www.paypal.com/donate/?hosted_button_id=53SQPWUPYSYQQ",
+          position: "right",
+          className: "navbar__item--give-desktop",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Resources',
+          title: "Resources",
           items: [
             {
-              label: 'AncientPaths',
-              to: '/docs/AncientPaths',
+              label: "AncientPaths",
+              to: "/docs/AncientPaths",
             },
             {
-              label: 'My Son Rising',
-              to: '/my-son-rising',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Discord',
-              href: 'https://discord.gg/Tb478Cud',
+              label: "My Son Rising",
+              to: "/my-son-rising",
             },
           ],
         },
         {
-          title: 'More',
+          title: "Community",
           items: [
             {
-              label: 'Contact',
-              href: 'mailto:mail.4campus@gmail.com',
+              label: "Discord",
+              href: "https://discord.gg/Tb478Cud",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Contact",
+              href: "mailto:mail.4campus@gmail.com",
             },
           ],
         },
@@ -150,28 +148,8 @@ const config = {
     },
     scripts: [
       {
-        src: 'https://api.reftagger.com/v2/RefTagger.js',
+        src: "https://api.reftagger.com/v2/RefTagger.js",
         async: true,
-      },
-      {
-        src: 'https://api.reftagger.com/v2/RefTagger.js',
-        type: 'text/javascript',
-        innerHTML: `
-          var refTagger = {
-            settings: {
-              bibleVersion: 'ESV'
-            }
-          };
-
-          (function(d, t) {
-            var n=d.querySelector('[nonce]');
-            refTagger.settings.nonce = n && (n.nonce || n.getAttribute('nonce'));
-            var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
-            g.src = 'https://api.reftagger.com/v2/RefTagger.js';
-            g.nonce = refTagger.settings.nonce;
-            s.parentNode.insertBefore(g, s);
-          }(document, 'script'));
-        `,
       },
     ],
   },
