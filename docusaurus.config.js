4fb7194a7c59e440ed399c8a59f43286dcce71ff
4campus.net <mail.4campus@gmail.com>
2025-09-20 11:18:40 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,7 +105,7 @@ const config = {
         {
           title: 'Resources',
           items: [
-            { label: 'My Son Rising', to: '/docs/AncientPaths' },
+            { label: 'Ancient Paths', to: '/docs/AncientPaths' },
             { label: 'My Son Rising', to: '/my-son-rising' },
           ],
         },
